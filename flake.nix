531c4567802849b7372ae5eda39fe8f81c78bf4c
C0D3 M4513R <28912031+C0D3-M4513R@users.noreply.github.com>
2025-02-25 21:29:35 +0100

    Add WorkDir back?

--- flake.nix
+++ flake.nix
@@ -83,8 +83,13 @@
             copyToRoot = [ package ];
 
             config = {
+                WorkingDir = "/data";
                 Cmd = [ "${nixpkgs.lib.getExe package}" ];
             };
+
+            extraCommands = ''
+                mkdir -p  $out/data
+            '';
 
             created = "${builtins.substring 0 4 self.lastModifiedDate}-${builtins.substring 4 2 self.lastModifiedDate}-${builtins.substring 6 2 self.lastModifiedDate}T${builtins.substring 8 2 self.lastModifiedDate}:${builtins.substring 10 2 self.lastModifiedDate}:${builtins.substring 12 2 self.lastModifiedDate}Z";
         };
